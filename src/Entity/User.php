bee4479bcf32193f8de693cdaf7c03ec530b22bb
yoann collot <collotyoann@homtail.fr>
2021-10-06 14:45:09 +0200

    forms user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -54,6 +54,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $formation;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstName;
+
     public function __construct()
     {
         $this->articles = new ArrayCollection();
@@ -220,4 +230,31 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+
 }
