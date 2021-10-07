99f489ffa5c518681b1be9caa8071488d43aca63
Noémie LECHERF <noemie.lecherf@epsi.fr>
2021-10-07 16:57:28 +0200

    formulaires propres + texte en francçais + affichage article sur accueil dans card

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\Article1Type;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +14,12 @@ class IndexController extends AbstractController
     /**
      * @Route("/home", name="index")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'articles' => $articleRepository->findAll(),
+
         ]);
     }
 }
